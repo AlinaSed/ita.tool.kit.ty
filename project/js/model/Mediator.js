b8d427cd6f53f002ff311088514abfe3430fd675
Sergey Purkar <yourbillc@gmail.com>
2017-07-20 21:11:49 +0300

    add mediator

---
+++ project/js/model/Mediator.js
@@ -0,0 +1,21 @@
+'use strict';
+
+class Mediator {
+    constructor () {
+        this.channels = {};
+    }
+
+    pub (channel, data) {
+        if (this.channels[channel]) {
+            this.channels[channel].forEach((fn) => fn(data));
+        }
+    }
+
+    sub (channel, fn) {
+        if (!this.channels[channel]) {
+            this.channels[channel] = [];
+        }
+
+        this.channels[channel].push(fn);
+    }
+}
