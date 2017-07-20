b2fcba1c3c11f82f26927aa273b330a2228452a6
Ihor Pavlenko <mr.pavlenko20@gmail.com>
2017-07-20 11:38:20 +0300

    SettingsCtrl

--- project/js/controller/settingsController.js
+++ project/js/controller/settingsController.js
@@ -1,5 +1,24 @@
 'use strict';
 
 export class SettingsController {
+	constructor (direction) {
+        this.direction = direction;
+        this.directionNames;
+    }
+
+    directionNames () {
+        this.direction.forEach((item)=>{
+            let names.push[item.name]
+        });
+        return names;
+    }
+
+    directionTests (name) {
+        this.direction.forEach((item)=>{
+            if (item.name !== name) continue;
+            let result = item.testList;
+        });
+        return result;
+    }
 
 }
