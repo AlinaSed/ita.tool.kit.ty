a0f7bda01d21c8dd214a0a662788099486f15841
AlinaSed <alina.sedletska@gmail.com>
2017-07-26 14:11:32 +0300

    Settings tpls modified; settings functional added

--- project/js/view/tpl/tplModalSettings.js
+++ project/js/view/tpl/tplModalSettings.js
@@ -2,18 +2,79 @@
 
 let tpl = {};
 
-tpl.modalSettings = `
-        <div id="settings">
+tpl.settingsModal =  `
+        <div class="settings-main">
             <div class="direction-select"></div>
             
-            <p><button class="show-test-list">Tests</button></p>
-            <p><button class="show-filter-list">Filters</button></p>
+            <div class="t-btn"><img src="images/t.png" class="show-test-list"/></div>
+            <div class="f-btn"><img src="images/f.png" class="show-filter-list"/></div>
             
-            <div id="tests-filters-container"></div>
-            <p><button class="close-button">Close</button></p>
+            <div class="tests-filters-container direction-list"> </div>
+            <div class="close-btn">
+                <i class="fa fa-window-close close-button close-btn" aria-hidden="true"></i>
+            </div>
+        </div>
+        <div class="settings-add-filter"> 
+
         </div>
 `;
 
+tpl.settingsDropDown = (directionList) => {
+    let optionList = '';
+
+    directionList.forEach((direction, i) => {
+        optionList += `
+            <option ${(i = 0) ? 'selected' : ''} value="${direction.name}">
+                ${direction.name}
+            </option>
+        `;
+    });
+
+    return `
+            <select class="settings-directions-select direction"> 
+                ${optionList} 
+                <option value="addDirection"> 
+                    <i class="fa fa-plus-circle" aria-hidden="true"></i> 
+                </option>   
+            </select>
+    `; 
+};
+
+tpl.settingsTests = (testList) => {
+    let tpl = '';
+   
+    testList.forEach((test) => tpl += `<li><a>${test.name}</a></li>`);
+    return `
+        <div class="wrapper-list" >Tests
+            <ul class="t-f-list">
+                ${tpl}
+            </ul>
+            <div class="new-test-input"> </div>
+            <i class="fa fa-plus-circle add-test" aria-hidden="true"></i>
+        </div>
+    `; 
+};
+
+tpl.settingsAddTest = `
+    <input type="text" class="new-test-name" value="new test">
+    <input type="text" class="new-test-max" value="100">
+    <button class="save-new-test">ok</button>
+`;
+
+tpl.settingsFilters = (filterList) => {
+    let tpl = '';
+
+    filterList.forEach((filter) => tpl += `<li><a>${filter.name}</a></li>`);
+
+    return `
+        <div class="wrapper-list">Filters
+            <ul class="t-f-list">
+                ${tpl}
+            </ul>
+            <i class="fa fa-plus-circle add-filter" aria-hidden="true"></i>
+        </div>
+    `; 
+};
 
 tpl.editGroupExamModal =
     `<div class="modal fade display in add-group-modal">
