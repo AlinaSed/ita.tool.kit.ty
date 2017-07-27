254a39feeb119a5e357162bf8a6bb4bbbee13551
Игорь Павленко <mr.pavlenko20@gmail.com>
2017-07-28 00:36:10 +0300

    days

---
+++ project/js/view/dayItemView.js
@@ -0,0 +1,45 @@
+'use strict';
+
+let mediator = require('../Mediator.js');
+
+class DayItemView {
+    constructor(day) {
+        this.day = day;
+        this.container = document.querySelector(this.selectors.daySection);
+        this.tplConteiner = '<div class="group-item col-xs-2 panel panel-primary">';
+    }
+
+    get selectors () {
+        return {
+            daySection: '.test-days'
+        };
+    }
+
+    renderDay () {
+        let template = this.tplConteiner + `<div class="panel-heading"><h3 class="panel-title">${this.day.date}</h3></div>`;
+
+        this.container.insertAdjacentHTML('afterBegin', template);
+    }
+
+    renderTimeSlot () {
+        let btn = '<i id="add-day" class="add-button fa fa-plus-circle" aria-hidden="true"></i>',
+            slotContainer = this.container.querySelector('.group-item');
+
+        if(this.day.time){
+            this.day.time.forEach ((timeSlot)=>{
+                let template = `<div class="panel-body"><div class="time-slot">${timeSlot}</div></div>`;
+
+                slotContainer.insertAdjacentHTML ('beforeend', template);
+            });
+        }
+
+        slotContainer.insertAdjacentHTML ('afterend', btn);
+    }
+
+    selectGroupForDay () {
+        mediator.pub ('group:selected', this.currentGroup);
+    }
+}
+
+module.exports = DayItemView;
+
