177fc5e007b7eba56af164fbddbcff7683d22c6f
Игорь Павленко <mr.pavlenko20@gmail.com>
2017-07-28 00:31:36 +0300

    days

--- project/js/prefilledGroups.js
+++ project/js/prefilledGroups.js
@@ -2,6 +2,7 @@
 
 let Group = require('./model/Group.js'),
 	Test = require('./model/Test.js'),
+    	Day = require('./model/Day.js'),
     prefilledDirection = require('./prefilledDirection.js');
 
 let prefilledGroups;
@@ -40,10 +41,12 @@ testListUI.push(new Test('Essay JS', 600));*/
 //prefilledDirection[2].testList = testListUI;
 //prefilledDirection[2].filterList = ['Filter1 ui', 'Filter2 ui'];
 
+let prefilledDay = [new Day('07/09/2017', ['12:30']), new Day('08/04/2017', ['13:30']), new Day('06/05/2017', ['14:30'])];
+
 prefilledGroups = [
-    new Group('Dp-120', prefilledDirection[0]),
-    new Group('Dp-112', prefilledDirection[1]),
-    new Group('Dp-117', prefilledDirection[2])
+    new Group('Dp-120', prefilledDirection[0], prefilledDay[0] ),
+    new Group('Dp-112', prefilledDirection[1], prefilledDay[1]),
+    new Group('Dp-117', prefilledDirection[2], prefilledDay[2])
 ];
 
 module.exports = prefilledGroups;
