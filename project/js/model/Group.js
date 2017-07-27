556b04de1c53fe6289c20d7b0a41955262491445
Игорь Павленко <mr.pavlenko20@gmail.com>
2017-07-28 00:50:38 +0300

    days

--- project/js/model/Group.js
+++ project/js/model/Group.js
@@ -2,11 +2,12 @@
 let Observer = require('../Event.js');
 
 class Group {
-    constructor (name, direction) {
+    constructor (name, direction, day) {
         this.name = name;
         this.direction = direction.name;
         this.testList = direction.testList;
         this.filterList = direction.filterList;
+        this.day = day;
 
         this.testAdded = new Observer(this);
     }
