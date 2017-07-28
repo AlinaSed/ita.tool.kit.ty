1a05f15583264ceaad8dc24b0edddc462ac4a718
AlinaSed <alina.sedletska@gmail.com>
2017-07-28 13:46:36 +0300

    Some refactoring Settings

--- project/js/view/SettingsAddFilterView.js
+++ project/js/view/SettingsAddFilterView.js
@@ -5,32 +5,31 @@ let tplSettings = require('./tpl/tpl.settings.js'),
 
 class SettingsAddFilterView {
     constructor (direction) {
-        this.direction = direction;
         this.modalContainer = document.querySelector('.modal-container');
 
         this.renderAddFilterModal(direction);
-        this.activate();
     }
 
     renderAddFilterModal (direction) {
-        let testListContainer,
-            testListForFilter = [],
+        let testListForFilter = [],
             selectedTestsContainer,
+            testListContainer,
+            saveFilterButton,
             filterNameInput,
-            actionSelect,
             conditionSelect,
-            gradeInput,
-            saveFilterButton;
+            actionSelect,
+            gradeInput;
 
         this.modalContainer.innerHTML = tplSettings.addFilter(direction);
-        testListContainer = this.modalContainer.querySelector('.tests-for-filter');
-        selectedTestsContainer = this.modalContainer.querySelector('.selected-tests');
-        filterNameInput = this.modalContainer.querySelector('.new-filter-name');
-        actionSelect = this.modalContainer.querySelector('.filter-action');
-        conditionSelect = this.modalContainer.querySelector('.filter-condition');
-        gradeInput = this.modalContainer.querySelector('.filter-grade');
-        saveFilterButton = this.modalContainer.querySelector('.save-new-filter');
 
+        selectedTestsContainer = this.modalContainer.querySelector('.selected-tests');
+        testListContainer = this.modalContainer.querySelector('.tests-for-filter');
+        saveFilterButton = this.modalContainer.querySelector('.save-new-filter');
+        filterNameInput = this.modalContainer.querySelector('.new-filter-name');
+        conditionSelect = this.modalContainer.querySelector('.filter-condition');
+        actionSelect = this.modalContainer.querySelector('.filter-action');
+        gradeInput = this.modalContainer.querySelector('.filter-grade');
+        
         testListContainer.addEventListener('click', (event) => {
             let testName = event.target.innerHTML,
                 test = direction.testList.find((test) => testName === test.name);
@@ -43,6 +42,7 @@ class SettingsAddFilterView {
 
         saveFilterButton.addEventListener('click', () => {
             let filterName, action, condition, grade;
+                //options = {};
 
             filterName = filterNameInput.value;
             action = actionSelect.options[actionSelect.selectedIndex].value;
@@ -52,10 +52,6 @@ class SettingsAddFilterView {
             direction.addFilter(testListForFilter, action, condition, grade, filterName);
             mediator.pub('filter:created');
         }, false);
-    }
-
-    activate () {
-        console.log('activate');
     }
 }
 
--- project/js/view/settingsView.js
+++ project/js/view/settingsView.js
@@ -19,7 +19,7 @@ class SettingsView {
             buttonAddDirection,
             closeSettings;
 
-        darkLayer.id = 'shadow'; //darkLayer.classList.add('shadow');
+        darkLayer.classList.add('shadow');
         document.body.appendChild(darkLayer);
         this.modalContainer.innerHTML = tplSettings.Modal; 
         this.modalContainer.style.display = 'block'; 
@@ -113,31 +113,112 @@ module.exports = SettingsView;
 
 
 
+
+
+
 /*
+class SettingsView {
+    constructor (settings) {
+        this.settings = settings;
+        this.modalContainer = document.querySelector('.modal-container');
+
+     /*   this.showSettingsWindow();
+        this.renderDirectionNames();
+        this.activate();
+    }
+
+    showSettingsWindow () {
+        let darkLayer = document.createElement('div'),
+            buttonClose,
+            buttonAddDirection,
+            closeSettings;
+
+        darkLayer.classList.add('shadow');
+        document.body.appendChild(darkLayer);
+        this.modalContainer.innerHTML = tplSettings.Modal; 
+        this.modalContainer.style.display = 'block'; 
+        buttonClose = this.modalContainer.querySelector('.close-button');
+        buttonAddDirection = this.modalContainer.querySelector('.add-direction');
+
+        closeSettings = () => {
+            darkLayer.parentNode.removeChild(darkLayer);
+            this.modalContainer.style.display = 'none';
+        };
+        
+        darkLayer.addEventListener('click', closeSettings, false);
+        buttonClose.addEventListener('click', closeSettings, false);
+        buttonAddDirection.addEventListener('click', () => {
+            let inputNewDirection = this.modalContainer.querySelector('.input-new-direction');
+
+            this.settings.addDirection(inputNewDirection.value);
+            inputNewDirection.value = '';
+            mediator.pub('direction:created');
+        }, false);
+    }
+
+    renderDirectionNames () {
+        let directionSelect = this.modalContainer.querySelector('.direction-select');
+
+        directionSelect.innerHTML = tplSettings.DropDown(this.settings.directionList);
+    }
+
+    activate () {
+        let elSelect = this.modalContainer.querySelector('.settings-directions-select'),
+            buttonFilters = this.modalContainer.querySelector('.show-filter-list'),
+            buttonTests = this.modalContainer.querySelector('.show-test-list');
+
+        elSelect.addEventListener('change', () => {
+            let selectedDirectionName;
+
+            selectedDirectionName = elSelect.options[elSelect.selectedIndex].value;
+            this.renderTests(selectedDirectionName);
+        }, false);
+
+        buttonTests.addEventListener('click', () => {
+            let selectedDirectionName = elSelect.options[elSelect.selectedIndex].value;
+            
+            this.renderTests(selectedDirectionName);
+        }, false);
+
+        buttonFilters.addEventListener('click', () => {
+            let selectedDirectionName = elSelect.options[elSelect.selectedIndex].value;
+
+            this.renderFilters(selectedDirectionName);
+        }, false);
+    }
+
     renderTests (directionName) {
         let listContainer = this.modalContainer.querySelector('.tests-filters-container'),
-            buttonAddTest,
+            testNameInput,
+            buttonSaveTest,
             direction;
 
         direction = this.settings.directionList.find((direction) => directionName === direction.name);
         listContainer.innerHTML = tplSettings.Tests(direction.testList);  
 
-        buttonAddTest = this.modalContainer.querySelector('.add-test');
-        buttonAddTest.addEventListener('click', () => { 
-            let addTestContainer = this.modalContainer.querySelector('.new-test-input'),
-                testNameInput, 
-                newTestMaxGrade, 
-                buttonSaveTest;
-
-            addTestContainer.innerHTML = tplSettings.AddTest;
-            testNameInput = addTestContainer.querySelector('.new-test-name');
-            newTestMaxGrade = addTestContainer.querySelector('.new-test-max');
-            buttonSaveTest = addTestContainer.querySelector('.save-new-test');
-
-            buttonSaveTest.addEventListener('click', () => {
-                direction.addTest(testNameInput.value);
-                mediator.pub('test:created', directionName);
-            }, false);
-
+        buttonSaveTest = this.modalContainer.querySelector('.save-new-test');
+        testNameInput = this.modalContainer.querySelector('.new-test-name');
+        
+        testNameInput.addEventListener('focus', () => buttonSaveTest.disabled = false);
+        buttonSaveTest.addEventListener('click', () => {
+            direction.addTest(testNameInput.value);
+            mediator.pub('test:created', directionName);
         }, false);
-    }*/
+    }
+
+    renderFilters (directionName) {
+        let listContainer = this.modalContainer.querySelector('.tests-filters-container'),
+            buttonAddFilter,
+            direction;
+
+        direction = this.settings.directionList.find((direction) => directionName === direction.name);
+        listContainer.innerHTML = tplSettings.Filters(direction.filterList);  
+        buttonAddFilter = this.modalContainer.querySelector('.add-filter');
+
+        buttonAddFilter.addEventListener('click', () => {
+            console.log('add filter');
+            mediator.pub('open:add-filter-modal', direction);
+        }, false);
+    }
+}
+*/
